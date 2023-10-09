4e62d67182f0f0c2f0fc78e66f2d1bc8c0caaa8b
Max <web.egorovm@gmail.com>
2023-10-09 19:51:17 +0700

    js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-// const cities = {
-//   4047198: 'Brighton',
-//   4697070: 'Henderson County',
-//   5437682: 'San Luis',
-//   5435464: 'Pueblo',
-//   5188029: 'East Pittsburgh',
-// };
+const URL = 'http://api.openweathermap.org/data/2.5/';
+const APIKEY = '3c2ab57f9f8c15dd6a412a2691437821';
 
 const URLMap = 'https://www.mapquestapi.com/staticmap/v5/';
 const APIKEYMap = 'BFGCW1QUqYWdZ5MdzIyaDQkr0ti1jvZG';
 
-// const { APIKEY } = require('git-variables');
-
-// const selectWrapper = document.querySelector('.out__select-wrapper');
+const URLGeoCod = 'https://api-bdc.net/data/';
+const APIKEYGeoCod = 'bdc_459d13c40cb94532841b7bd393af14e1';
+// cansel config.js ====================================================
 const out = document.querySelector('.out');
 const metricSel = document.querySelector('#metric');
 
 // weather data output===================================================
-
 let divImg = out.querySelector('div.out__icon');
 let temp = out.querySelector('h3.out__temp');
 let nameCity = out.querySelector('.out__name-city');
@@ -88,55 +82,6 @@ async function showPosWeatForecast(position) {
   document.getElementById('mapholder').innerHTML =
     "<img src='" + imgUrlMap + "'>";
 }
-
-// // create select ==================================================
-// function createSel(cities) {
-//   const select = document.createElement('select');
-//   select.classList.add('main-weather__city-selection');
-//   select.setAttribute('id', 'city');
-
-//   for (let key in cities) {
-//     let option = document.createElement('option');
-//     option.value = key;
-//     option.innerHTML = cities[key];
-//     // select.append(option);
-//     select.add(option, null);
-//   }
-
-//   selectWrapper.prepend(select);
-// }
-// createSel(cities);
-
-// weather data ==================================================
-
-// async function getWeather() {
-//   // const cityId = document.querySelector('#city').value;
-//   // console.log(lat, lon);
-//   // const paramFetchWeatcher = `weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`;
-//   // // const paramFetchWeatcher = `weather?id=${cityId}&units=metric&appid=${APIKEY}`;
-//   // const paramFetchForecast = `forecast?id=${cityId}&units=metric&appid=${APIKEY}`;
-//   // // const paramFetchForecast = `forecast?id=${cityId}&units=metric&appid=${APIKEY}`;
-//   // const resWeatcher = await fetch(URL + paramFetchWeatcher);
-//   // const resForecast = await fetch(URL + paramFetchForecast);
-//   // const dataWeather = await resWeatcher.json();
-//   // const dataForecast = await resForecast.json();
-//   // console.log(dataWeather);
-//   // console.log(dataForecast);
-//   // showWeather(dataWeather);
-//   // showForecast(dataForecast);
-//   // fetch=================================================
-//   // fetch(param.URL + paramFetch)
-//   //   .then((weather) => {
-//   //     return weather.json();
-//   //   })
-//   //   .then(showWeather)
-//   //   .catch(function () {
-//   //     //catch any errors
-//   //   });
-// }
-// getWeather();
-
-// document.querySelector('#city').onchange = getWeather;
 
 function showWeather(dataW, dataGeoPos) {
   let dateUnixToDate = convertUnixToDate(dataW.timezone, dataW.dt);
