ac4cd5fae7e560d1c674f48b89a0f4202fce0afa
Max <web.egorovm@gmail.com>
2023-10-09 19:02:27 +0700

    js

--- cfg-pass.js
+++ cfg-pass.js
@@ -4,8 +4,8 @@
 // dotenv.config();
 // console.log(process.env);
 
-import dotenvFlow from './node_modules/dotenv-flow';
-dotenvFlow.config();
+// import dotenvFlow from './node_modules/dotenv-flow';
+// dotenvFlow.config();
 
 const URL = 'http://api.openweathermap.org/data/2.5/';
 // const APIKEY = '3c2ab57f9f8c15dd6a412a2691437821';
