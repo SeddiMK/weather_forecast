633d8b3cf574bd114b16932d117611b109586691
Max <web.egorovm@gmail.com>
2024-03-25 14:05:27 +0300

    config-fix

--- config.js
+++ config.js
@@ -7,14 +7,26 @@
 // import dotenvFlow from './node_modules/dotenv-flow';
 // dotenvFlow.config();
 
-const URL = 'http://api.openweathermap.org/data/2.5/';
-const APIKEY = '3c2ab57f9f8c15dd6a412a2691437821';
-// const APIKEY = process.env.API_KEY;
+export const URL = {
+  URL_OWM: 'http://api.openweathermap.org/data/2.5/',
+  URLGeoCod: 'https://api-bdc.net/data/',
+  URLMap: 'https://www.mapquestapi.com/staticmap/v5/',
+};
 
-const URLGeoCod = 'https://api-bdc.net/data/';
-const APIKEYGeoCod = 'bdc_459d13c40cb94532841b7bd393af14e1';
-// const APIKEYGeoCod = process.env.API_KEYGeoCod;
+export const API_KEY = {
+  APIKEY_OWM: '3c2ab57f9f8c15dd6a412a2691437821',
+  APIKEYGeoCod: 'bdc_459d13c40cb94532841b7bd393af14e1',
+  APIKEYMap: 'BFGCW1QUqYWdZ5MdzIyaDQkr0ti1jvZG',
+};
 
-const URLMap = 'https://www.mapquestapi.com/staticmap/v5/';
-const APIKEYMap = 'BFGCW1QUqYWdZ5MdzIyaDQkr0ti1jvZG';
-// const APIKEYMap = process.env.API_KEYMap;
+// export const URL = 'http://api.openweathermap.org/data/2.5/';
+// export const APIKEY = '3c2ab57f9f8c15dd6a412a2691437821';
+//export const APIKEY = process.env.API_KEY;
+
+// export const URLGeoCod = 'https://api-bdc.net/data/';
+// export const APIKEYGeoCod = 'bdc_459d13c40cb94532841b7bd393af14e1';
+//export const APIKEYGeoCod = process.env.API_KEYGeoCod;
+
+// export const URLMap = 'https://www.mapquestapi.com/staticmap/v5/';
+// export const APIKEYMap = 'BFGCW1QUqYWdZ5MdzIyaDQkr0ti1jvZG';
+//export const APIKEYMap = process.env.API_KEYMap;
